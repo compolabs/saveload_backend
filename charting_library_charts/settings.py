ada59f5106daf52a01d830c5bcecfec146e71fb3
Bejoy <casperyourweb@gmail.com>
2023-05-01 04:29:13 +0300

    add host

--- charting_library_charts/settings.py
+++ charting_library_charts/settings.py
@@ -25,7 +25,10 @@ DATABASES = {
 	}
 }
 
-
+ALLOWED_HOSTS = [
+	'62.109.15.24',
+	'127.0.0.1'
+]
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
