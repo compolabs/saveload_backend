8787431ac8b6b3b947b5b58dea726610c4e9582f
Bejoy <casperyourweb@gmail.com>
2023-05-02 19:33:07 +0300

    fix host

--- charting_library_charts/settings.py
+++ charting_library_charts/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
 
 ALLOWED_HOSTS = [
 	'127.0.0.1',
-	'172.26.5.107'
+	'13.36.170.111'
 ]
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
